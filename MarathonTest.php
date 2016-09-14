25e272047d6522629a1b478fd60479a362b8eed1
frogconn <frogconn@gmail.com>
2016-09-14 14:52:20 +0700

    Added test case check nettime is not show up

--- MarathonTest.php
+++ MarathonTest.php
@@ -4,6 +4,24 @@ require "ConverterTest.php";
 
 class Runner
 {
+
+	private function convertTimeToSecond($time){
+		$SEVEN_HOURS = 25200;
+        $NO_RUNING_TIME = 0;
+
+        $convert = new Converter($time);
+        $second = $convert->toSecond();
+
+        if ($second == $NO_RUNING_TIME) {
+            return -1;
+        } else if ($second > $SEVEN_HOURS) {
+            return 0;
+        } else if ($second > $NO_RUNING_TIME && $second < $SEVEN_HOURS) {
+            return $second;
+        }
+
+        return 0;
+	}
     public function setFinishTime($finishTime)
     {
         $this->finishTime = $finishTime;
@@ -14,50 +32,44 @@ class Runner
         $this->netTime = $netTime;
     }
 
+
     public function getFinishTimeInSecond()
     {
-        $SEVEN_HOURS = 25200;
-        $NO_RUNING_TIME = 0;
+        return $this->convertTimeToSecond($this->finishTime);
+    }
 
-        $convert = new Converter($this->finishTime);
-        $second = $convert->toSecond();
-        
-        if ($second == $NO_RUNING_TIME) {
-            return -1;
-        } else if ($second > $SEVEN_HOURS) {
-            return 0;
-        } else if ($second > $NO_RUNING_TIME && $second < $SEVEN_HOURS) {
-            return $second;
-        }
-
-        return 0;
+    function getNetTimeInSecond(){
+    	return $this->convertTimeToSecond($this->netTime);
     }
 }
 
 class MarathonTest extends PHPUnit_Framework_TestCase
 {
+    public function testRunnerNetTimeIsNotShowup()
+    {
+        $runner = new Runner();
+        $runner->setNetTime("00:00:00");
+        $this->assertEquals(-1,$runner->getNetTimeInSecond());
+    }
 
-    public function testRunnerInTime()
+    public function testRunnerFinishTimeInTime()
     {
         $runner = new Runner();
         $runner->setFinishTime("02:01:01");
-        $runner->setNetTime("02:01:01");
         $this->assertEquals(7261, $runner->getFinishTimeInSecond());
     }
 
-    public function testRunnerIsNotShowup()
+    public function testRunnerFinishTimeIsNotShowup()
     {
         $runner = new Runner();
         $runner->setFinishTime("00:00:00");
-        $runner->setNetTime("00:00:00");
         $this->assertEquals(-1, $runner->getFinishTimeInSecond());
 
     }
-    public function testRunnerIsOverTimeReturnZero()
+    public function testRunnerFinishTimeIsOverTimeReturnZero()
     {
         $runner = new Runner();
         $runner->setFinishTime("07:00:00");
-        $runner->setNetTime("07:00:00");
         $this->assertEquals(0, $runner->getFinishTimeInSecond());
     }
 }
