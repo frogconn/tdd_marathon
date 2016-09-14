8d35ab8697dbbdaa0d52963db70a990a555834d7
frogconn <frogconn@gmail.com>
2016-09-14 14:32:33 +0700

    Test over time is 0

--- MarathonTest.php
+++ MarathonTest.php
@@ -2,29 +2,47 @@
 
 require "ConverterTest.php";
 
-class Runner{
-	function setFinishTime($finishTime){
-		$this->finishTime=$finishTime;
-	}
+class Runner
+{
+    public function setFinishTime($finishTime)
+    {
+        $this->finishTime = $finishTime;
+    }
 
-	function setNetTime($netTime){
-		$this->netTime=$netTime;
-	}
-	
-	function getFinishTimeInSecond(){
-		$convert = new Converter($this->finishTime);
-		return -1;
-	}	
+    public function setNetTime($netTime)
+    {
+        $this->netTime = $netTime;
+    }
+
+    public function getFinishTimeInSecond()
+    {
+        $convert = new Converter($this->finishTime);
+        if ($convert->toSecond() == 0) {
+            return -1;
+        } else if ($convert->toSecond() > 25200) {
+            return 0;
+        }
+
+        return 0;
+    }
 }
 
 class MarathonTest extends PHPUnit_Framework_TestCase
 {
 
-	function testRunnerIsNotShowup(){
-		$runner=new Runner();
-		$runner->setFinishTime("00:00:00");
-		$runner->setNetTime("00:00:00");
-		$this->assertEquals(-1,$runner->getFinishTimeInSecond());
+    public function testRunnerIsNotShowup()
+    {
+        $runner = new Runner();
+        $runner->setFinishTime("00:00:00");
+        $runner->setNetTime("00:00:00");
+        $this->assertEquals(-1, $runner->getFinishTimeInSecond());
 
-	}
+    }
+    public function testRunnerIsOverTimeReturnZero()
+    {
+        $runner = new Runner();
+        $runner->setFinishTime("07:00:00");
+        $runner->setNetTime("07:00:00");
+        $this->assertEquals(0, $runner->getFinishTimeInSecond());
+    }
 }
