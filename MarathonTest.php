09bb26985886de416cdea8e99cdc3ffe2b126d68
frogconn <frogconn@gmail.com>
2016-09-14 13:45:55 +0700

    create marathontest and runner class

---
+++ MarathonTest.php
@@ -0,0 +1,25 @@
+<?php
+class Runner{
+	function setFinishTime($finishTime){
+		$this->finishTime=$finishTime;
+	}
+
+	function setNetTime($netTime){
+		$this->netTime=$netTime;
+	}
+	
+	function getFinishTimeInSecond(){
+		return -1;
+	}	
+}
+
+class MarathonTest extends PHPUnit_Framework_TestCase
+{
+	function testRunnerIsNotShowup(){
+		$runner=new Runner();
+		$runner->setFinishTime("00:00:00");
+		$runner->setNetTime("00:00:00");
+		$this->assertEquals(-1,$runner->getFinishTimeInSecond());
+
+	}
+}
